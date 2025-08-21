6644b9e65e82e927443b455c0f3c2f72c2ae13c5
TheKany <skarnjs06@naver.com>
2025-08-21 12:31:19 +0900

    메타태그 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const siteUrl =
+    process.env.NEXT_PUBLIC_SITE_URL ?? "https://banana-fawn.vercel.app/";
+  const contentSrc = `${siteUrl}/img/contents-01.webp`;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="과제" />
+        <meta property="og:image" content={contentSrc} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={contentSrc} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,4 @@
-import { Viewport } from "next";
 import { Html, Head, Main, NextScript } from "next/document";
-
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1.0,
-  maximumScale: 1,
-  userScalable: false,
-};
 
 export default function Document() {
   return (
